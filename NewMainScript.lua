73eb963649574297f75714115a6d8eb33ffec649
DaiPlayz <101194864+DaiPlayz@users.noreply.github.com>
2024-09-23 14:55:16 -0400

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -14,10 +14,6 @@ local version = 1
 
 if not isfolder('vape') then makefolder('vape') end
 
-if game.Players.LocalPlayer.Name == 'IIllIIIlllIIIlllIllI' then
-	game.Players.LocalPlayer:Kick('skibidi dop dop dop yes yes - daiplayz :3')
-end
-
 
 local VWFunctions = {}
 function VWFunctions.CreateID()
